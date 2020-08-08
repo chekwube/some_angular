bf886c72c15e2b07b9a895abf4e1e0b2af037681
Okeke Chekwube <okekechekwube1@gmail.com>
2020-08-08 17:27:54 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    });
+  });
+
+  it('should create the app', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it(`should have a newTodo todo`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy()
+  }));
 
-  it(`should have as title 'todo-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should display "Todos" in h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todo-app app is running!');
-  });
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  }));
 });
---
+++ src/app/todo-data.service.spec.ts
@@ -0,0 +1,108 @@
+import { TestBed } from '@angular/core/testing';
+import {Todo} from './todo'
+
+import { TodoDataService } from './todo-data.service';
+
+describe('TodoDataService', () => {
+  let service: TodoDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TodoDataService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('#getAllTodos()', () => {
+
+    it('should return an empty array by default', () => {
+      expect(service.getAllTodos()).toEqual([]);
+    });
+
+    it('should return all todos', () => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    });
+
+  });
+
+  describe('#save(todo)', () => {
+
+    it('should automatically assign an incrementing id', () => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getTodoById(1)).toEqual(todo1);
+      expect(service.getTodoById(2)).toEqual(todo2);
+    });
+
+  });
+
+  describe('#deleteTodoById(id)', () => {
+
+    it('should remove todo with the corresponding id', () => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(1);
+      expect(service.getAllTodos()).toEqual([todo2]);
+      service.deleteTodoById(2);
+      expect(service.getAllTodos()).toEqual([]);
+    });
+
+    it('should not removing anything if todo with corresponding id is not found', () => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(3);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    });
+
+  });
+
+  describe('#updateTodoById(id, values)', () => {
+
+    it('should return todo with the corresponding id and updated data', () => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(1, {
+        title: 'new title'
+      });
+      expect(updatedTodo.title).toEqual('new title');
+    });
+
+    it('should return null if todo is not found', () => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(2, {
+        title: 'new title'
+      });
+      expect(updatedTodo).toEqual(null);
+    });
+
+  });
+
+  describe('#toggleTodoComplete(todo)', () => {
+
+    it('should return the updated todo with inverse complete status',() => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.toggleTodoComplete(todo);
+      expect(updatedTodo.complete).toEqual(true);
+      service.toggleTodoComplete(todo);
+      expect(updatedTodo.complete).toEqual(false);
+    });
+
+  });
+
+});
